85e16bff9068053bf1ccd60a17b65735b426f26e
ajojose803 <ajojose803@gmail.com>
2024-10-25 23:59:25 +0530

    Hero picker updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,9 @@ const Hero = () => {
 
   useEffect(() => {
     axios.get(endpoints.popular).then((response) => {
-        console.log(response.data);
+        const movies = response.data.results;
+        const randomMovie = movies[Math.floor(Math.random() * movies.length)];
+
     })
   })
     return (
