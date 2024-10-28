44882620bbdc2966347f6fc92701e010352bc789
ajojose803 <ajojose803@gmail.com>
2024-10-28 21:50:17 +0530

    Updated Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth} from "../context/AuthContext"
 
 const Login = () => {
+
+  const [rememberLogin, setRememberLogin] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { user, login} = UserAuth();
+  const navigate = useNavigate();
+
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await login(email, password)
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
-    <div>Login</div>
+    <>
+      <div className='w-full h-screen'>
+        <img
+          className=' hidden sm:block absolute w-full h-full object-cover'
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/74d734ca-0eab-4cd9-871f-bca01823d872/web/IN-en-20241021-TRIFECTA-perspective_2277eb50-9da3-4fdf-adbe-74db0e9ee2cf_small.jpg"
+          alt="///"
+        />
+        <div className='bg-black/70 fixed top-0 left-0 w-full h-screen' />
+
+
+        <div className='fixed w-full px-4 py-24 z-20'>
+          <div className='max-w-[450px] h-[600px] mx-auto bg-black/60 rounded-lg'>
+            <div className='max-w-[320px]  mx-auto py-16'>
+              <h1 className='text-3xl font-nsans-bold capitalize'> Login</h1>
+              <form
+                onSubmit={handleFormSubmit}
+                className='w-full flex flex-col py-4'>
+                <input className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder='email'
+                  autoComplete='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder='password'
+                  autoComplete='current-password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button className='p-3  my-6 bg-red-700 rounded font-nsans-bold '> Login</button>
+
+
+                <div className='flex justify-between items-center text-gray-600'>
+                  <p>
+                    <input
+                      type="checkbox"
+                      className='mr-2'
+                      checked={rememberLogin}
+                      onChange={(e) => setRememberLogin(!rememberLogin)}
+                    />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+
+                </div>
+                <p className='my-4'>
+                  <span className='text-gray-600 mr-2'> New to Netflix? </span>
+                  <Link to="/signup"> Sign Up </Link>
+                </p>
+
+              </form>
+
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth} from "../context/AuthContext"
 
 const Signup = () => {
+
+  const [rememberLogin, setRememberLogin] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate();
+
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signUp(email, password)
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <>
       <div className='w-full h-screen'>
@@ -16,15 +37,44 @@ const Signup = () => {
           <div className='max-w-[450px] h-[600px] mx-auto bg-black/60 rounded-lg'>
             <div className='max-w-[320px]  mx-auto py-16'>
               <h1 className='text-3xl font-nsans-bold capitalize'> Sign up</h1>
-              <form className='w-full flex flex-col py-4'>
-                <input className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder='email' autoComplete='email' />
-                <input className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder='password' autoComplete='current-password' />
+              <form
+                onSubmit={handleFormSubmit}
+                className='w-full flex flex-col py-4'>
+                <input className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder='email'
+                  autoComplete='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder='password'
+                  autoComplete='current-password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
                 <button className='p-3  my-6 bg-red-700 rounded font-nsans-bold '> Sign up</button>
+
+
                 <div className='flex justify-between items-center text-gray-600'>
-                  <p>remember</p>
-                  <p>remember</p>
-                  
+                  <p>
+                    <input
+                      type="checkbox"
+                      className='mr-2'
+                      checked={rememberLogin}
+                      onChange={(e) => setRememberLogin(!rememberLogin)}
+                    />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+
                 </div>
+                <p className='my-4'>
+                  <span className='text-gray-600 mr-2'> Already subcribed to Netflix? </span>
+                  <Link to="/login"> Sign in </Link>
+                </p>
+
               </form>
 
 
