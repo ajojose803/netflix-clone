dd5a2e39d914e006d062ad81d47d89a0c1fbfc81
ajojose803 <ajojose803@gmail.com>
2024-10-28 20:38:21 +0530

    Added Firebase for Authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,51 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Navbar = () => {
+
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className='absolute w-full p-4 flex items-center justify-between z-50'>
       <Link to='/'>
         <h1 className="uppercase text-red-600 font-nsans-bold cursor-pointer text-5xl"> netflix</h1>
       </Link>
 
-      <div>
-        <Link to='/login'>
-          <button className='capitalize pr-4'> login</button>
-        </Link>
-        <Link to='/signup'>
-          <button className='capitalize pr-4 bg-red-600 px-6 py-2 rounded cursor-pointer'> signup</button>
-        </Link>
-      </div>
+      {
+        user?.email ? (
+          <div>
+            <Link to='/profile'>
+              <button className='capitalize pr-4'> profile</button>
+            </Link>
+
+            <button onClick={handleLogout} className='capitalize pr-4 bg-red-600 px-6 py-2 rounded cursor-pointer'>
+              logOut
+            </button>
+          </div>
+
+        ) : (
+          <div>
+            <Link to='/login'>
+              <button className='capitalize pr-4'> login</button>
+            </Link>
+            <Link to='/signup'>
+              <button className='capitalize pr-4 bg-red-600 px-6 py-2 rounded cursor-pointer'> signup</button>
+            </Link>
+          </div>
+        )
+      }
+
     </div>
   )
 }
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+
+} from 'firebase/auth'
+
+import { auth, db } from '../services/firebase'
+
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+    function signUp(email, password) {
+        createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    function logOut() {
+        return signOut(auth);
+    }
+
+    return (
+        <AuthContext.Provider value={{user, signUp, login, logOut}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function UserAuth() {
+    return useContext(AuthContext);
+}
